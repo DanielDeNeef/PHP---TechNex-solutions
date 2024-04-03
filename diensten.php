b878424dc71d86d72bb372b2153c578d28ed3c06
daniel <daniel.deneef@gmail.com>
2024-04-03 17:17:46 +0200

    add services to page

--- diensten.php
+++ diensten.php
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diensten</title>
+<!-- header  -->
+<?php $pageTitle = 'Diensten' ?>
+<?php include 'includes/header.php' ?>
 
-    <link rel="stylesheet" href="styles/content.css">
-</head>
-<body>
+<!-- navbar -->
+<?php $currentPage = 'diensten' ?>
+<?php include 'includes/navbar.php' ?>
 
-    <!-- header  -->
-    <?php include 'includes/header.php' ?>
-    
-    <!-- navbar -->
-    <?php $currentPage = 'diensten' ?>
-    <?php include 'includes/navbar.php' ?>
-
-    <!-- content -->
-    <div class="content_wrapper">        
-        <div style="height:400px; width:90%; margin:auto; background-color: #b5b5b5e7;">
-            <h2 style="font-size: 40px; text-align:center">Diensten</h2>
-        </div>
+<!-- content -->
+<div class="content_wrapper">
+    <div style="height:auto; width:90%; margin:auto; background-color: #b5b5b5e7;">
+        <h2 style="font-size: 40px; text-align:center">Diensten</h2>
+        <?php include 'includes/service.php' ?>
     </div>
+</div>
 
-    <!-- Footer -->
-    <?php include_once 'includes/footer.php' ?>
-    
-</body>
-</html>
+<!-- Footer -->
+<?php include_once 'includes/footer.php' ?>
---
+++ includes/service.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" href="styles/service.css">
+
+<div class="service-container">
+    <div class="service-row">
+        <div class="service-column-66">
+            <h1 class="service-xlarge-font"><b>IT-consulting</b></h1>
+            <br>
+            <p>Wij bieden deskundig advies en begeleiding om uw IT-strategie te optimaliseren, van het verbeteren van de
+                operationele efficiëntie tot het ondersteunen van uw bedrijfsgroei.</p>
+            <br>
+            <button class="service-button" style="background-color:#333">Leer meer</button>
+        </div>
+        <div class="service-column-33">
+            <img src="resources/consulting.jpg" width="335" height="471">
+        </div>
+    </div>
+</div>
+
+<div class="service-container" style="background-color:#f1f1f1">
+    <div class="service-row">
+        <div class="service-column-33">
+            <img src="resources/transformation.jpg" alt="App" width="335" height="471">
+        </div>
+        <div class="service-column-66">
+            <h1 class="service-xlarge-font"><b>Digitale transformatie</b></h1>
+            <br>
+            <p>Wij helpen uw bedrijf bij de overgang naar digitale processen en tools, om zo de productiviteit te
+                verhogen en een betere klantervaring te bieden.</p>
+            <br>
+            <button class="service-button" style="background-color:#333">Leer meer</button>
+        </div>
+    </div>
+</div>
+
+<div class="service-container">
+    <div class="service-row">
+        <div class="service-column-66">
+            <h1 class="service-xlarge-font"><b>Cybersecurity</b></h1>
+            <br>
+            <p> Wij bieden oplossingen om uw bedrijf te beschermen tegen cyberdreigingen, inclusief risicobeoordeling,
+                implementatie van beveiligingsmaatregelen en incidentrespons.</p>
+            <br>
+            <button class="service-button" style="background-color:#333">Leer meer</button>
+        </div>
+        <div class="service-column-33">
+            <img src="resources/security.png" width="335" height="471">
+        </div>
+    </div>
+</div>
