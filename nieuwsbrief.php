8e2a0cba7eb99695c06715dd6b53852a6c84be0d
daniel <daniel.deneef@gmail.com>
2024-04-14 14:13:12 +0200

    created news letter page

---
+++ nieuwsbrief.php
@@ -0,0 +1,32 @@
+<!-- header  -->
+<?php $pageTitle="NewsLetter" ?>
+<?php include 'includes/header.php' ?>
+<link rel="stylesheet" href="styles/nieuwsbrief.css">
+
+<!-- navbar -->
+<?php $currentPage = 'nieuwsbrief' ?>
+<?php include 'includes/navbar.php' ?>
+
+<form action="action_page.php" method="post">
+    <div class="container">
+        <h2>Subscribe to our Newsletter</h2>
+        <p>Lorem ipsum..</p>
+    </div>
+
+    <div class="container" style="background-color:white">
+        <input type="text" placeholder="Name" name="name" required>
+        <input type="email" placeholder="Email address" name="mail" required>
+        <label>
+            <input type="checkbox" checked="checked" name="subscribe">
+            Daily Newsletter
+        </label>
+    </div>
+
+    <div class="container">
+        <input type="submit" value="Subscribe">
+    </div>
+</form>
+
+
+<!-- Footer -->
+<?php include_once 'includes/footer.php' ?>
