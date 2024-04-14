67efd0197f1b4954c9b9fee0859e6e67078484c8
daniel <daniel.deneef@gmail.com>
2024-04-14 14:27:05 +0200

    create registration page

--- action_page.php
+++ action_page.php
@@ -1,8 +1,22 @@
-<?php
-echo "<h1>Ingevoerde gegevens</h1>";
-foreach ($_POST as $key => $value) {
-    echo "<b>" . ($key) . "</b>: " . ($value) . "<br>";
-}
-?>
-<br>
-<button onclick="window.history.back()">Ga Terug</button>
+<!-- header  -->
+<?php $pageTitle="Registration" ?>
+<?php include 'includes/header.php' ?>
+
+<!-- navbar -->
+<?php $currentPage = 'registration' ?>
+<?php include 'includes/navbar.php' ?>
+
+<div style="margin : 20px;">
+    <?php
+    echo "<h1>Ingevoerde gegevens</h1>";
+    foreach ($_POST as $key => $value) {
+        echo "<b>" . ($key) . "</b>: " . ($value) . "<br>";
+    }
+    ?>
+    <br>
+    <button onclick="window.history.back()">Ga Terug</button>
+</div>
+
+
+<!-- Footer -->
+<?php include_once 'includes/footer.php' ?>
