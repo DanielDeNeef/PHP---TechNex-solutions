712a4727e915d25f1517b96aef9916a0705852d9
daniel <daniel.deneef@gmail.com>
2024-04-21 16:16:45 +0200

    save newsletter in csv

--- controllers/registration_ctrl.php
+++ controllers/registration_ctrl.php
@@ -1,20 +1,20 @@
 <?php
 
-// Get the form data
-$email = $_POST['email'];
-$password = $_POST['psw'];
-$repeatPassword = $_POST['pswrepeat'];
-$remember = isset($_POST['remember']) ? 'Yes' : 'No';
+    // Get the form data
+    $email = $_POST['email'];
+    $password = $_POST['psw'];
+    $repeatPassword = $_POST['pswrepeat'];
+    $remember = isset($_POST['remember']) ? 'Yes' : 'No';
 
-// Open the CSV file, or create it if it doesn't exist
-$file = fopen('../resources/registrations.csv', 'a');
+    // Open the CSV file, or create it if it doesn't exist
+    $file = fopen('../resources/registrations.csv', 'a');
 
-// Write the form data to the CSV file
-fputcsv($file, [$email, $password, $remember],";");
+    // Write the form data to the CSV file
+    fputcsv($file, [$email, $password, $remember],";");
 
-// Close the CSV file
-fclose($file);
+    // Close the CSV file
+    fclose($file);
 
-// Redirect back to the registration page (or wherever you want)
-header('Location: ../profile.php');
+    // Redirect back to the registration page (or wherever you want)
+    header('Location: ../profile.php');
 ?>
